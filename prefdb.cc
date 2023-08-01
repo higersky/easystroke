5f5b45411b0295ffdb4e430151b0c28ae3f85dd2
higersky <64680426+higersky@users.noreply.github.com>
2023-08-01 15:33:59 +0800

    Don't ignore xshape setting when saving. * imported from https://aur.archlinux.org/cgit/aur.git/tree/dont-ignore-xshape-when-saving.patch?h=easystroke-git

--- prefdb.cc
+++ prefdb.cc
@@ -72,8 +72,7 @@ template<class Archive> void PrefDB::serialize(Archive & ar, const unsigned int 
 		ar & help;
 	}
 	ar & trace.unsafe_ref();
-	if (trace.get() == TraceShape)
-		trace.unsafe_ref() = TraceDefault;
+
 	if (version < 3) {
 		int delay{};
 		ar & delay;
